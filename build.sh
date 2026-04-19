06b1adce2303b0de5848f8c2ac22a1651a7f6fc3
BigKozlowski <dmitriykozlov1994@gmail.com>
2026-04-19 16:03:31 +0300

    fix: build

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ SDL_LIBS=$(pkg-config --libs sdl2)
 echo "SDL_CFLAGS: $SDL_CFLAGS"
 echo "SDL_LIBS: $SDL_LIBS"
 
-mlton -default-ann 'allowFFI true' -cc-opt "$SDL_CFLAGS" -link-opt "$SDL_LIBS" hello_sdl.mlb c_exports.c
+mlton -default-ann 'allowFFI true' \
+    -cc-opt "$SDL_CFLAGS" -link-opt "$SDL_LIBS" \
+    hello_sdl.mlb c_exports.c
